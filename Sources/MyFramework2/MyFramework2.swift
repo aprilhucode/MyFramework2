f2778aec9e290aef1677f55eb768e3ef813a8aca
April Huang <phuang@beaconfireinc.com>
2024-03-15 09:26:27 -0400

    Add auth

--- Sources/MyFramework2/MyFramework2.swift
+++ Sources/MyFramework2/MyFramework2.swift
@@ -1,7 +1,23 @@
 import Foundation
 
 public struct MyFramework2 {
+    private static var isAPIKeyValid = false
+    
+    private static let apiKeys = ["private_key_123", "private_key_456"]
+    
+    private static func verifyAPIKey() -> Bool {
+        if !isAPIKeyValid {
+            print("Please provide a valid API key")
+        }
+        return isAPIKeyValid
+    } 
+    
+    public static func configure(apiKey: String) {
+        isAPIKeyValid = apiKeys.contains(apiKey)
+    }
+    
     public static func logTime() {
+        guard verifyAPIKey() else { return }
         print("The time is \(Date())")
     }
 }
